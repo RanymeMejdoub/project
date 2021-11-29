659a3c0d04eb191e3f11540ac239841375a40685
ranyme mejdoub <ranymee@icloud.com>
2021-11-29 12:13:11 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-
+import './style.css';
 import './App.css';
-
+import myearth from './earthh.jpg'
 function App() {
   return (
     <div className="App">
-      <div style={{border: 'solid 1px black' maxWidth: '100vw'}}>
-      <h1> className="title red">project </h1>
+      <div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
+      <h1 className="titleRed">  project </h1>
       <br />
       < img src="moonn.jpg" alt="this is the moon" width="400"/>
       <br />
-      <img src="earthh.jpg" alt="this is the earth" width="400"></img>      
+      <img src={myearth} alt="this is the earth" width="400"></img>      
       </div> 
       
     </div>
